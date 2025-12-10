e019f4fa4b74121f575d3f9c49b323d6928ef683
Harth Majeed <harthmajeed@hotmail.com>
2025-12-09 23:02:31 -0500

    made some file structure changes

--- cybulde/__init__.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-warnings.filterwarnings(action="ignore", category=RuntimeWarning, module=".*schema.*")
-
-from cybulde.config_schemas.experiment.bert import local_bert  # noqa: E402
-
-__all__ = ["local_bert"]
--- cybulde/dataset_checking.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-test_df = pd.read_parquet("gs://cybulde/data/processed/rebalanced_splits/dev.parquet")
-
-print(test_df.shape)
-print(test_df.columns.values)
-
-samples_per_dataset = test_df.groupby("dataset_name").size()
-print(samples_per_dataset)
